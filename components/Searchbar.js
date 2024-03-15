38fa75d2630f3503da720d1518167f4af21b3a30
TharunL06 <tharunlakshmanan06@gmail.com>
2024-03-15 14:27:33 +0530

    removed

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -4,42 +4,11 @@ import IconButton from '@mui/material/IconButton';
 import ClearIcon from '@mui/icons-material/Clear';
 
 const SearchBar = ({ onSearch }) => {
-    const [searchQuery, setSearchQuery] = useState('');
-
-    const handleChange = (event) => {
-        const query = event.target.value;
-        setSearchQuery(query);
-        onSearch(query);
-    };
-
-    const handleClear = () => {
-        setSearchQuery('');
-        onSearch('');
-    };
+    
 
     return (
-        <div style={{ position: 'relative' }}>
-            <TextField
-                label="Search"
-                variant="outlined"
-                fullWidth
-                value={searchQuery}
-                onChange={handleChange}
-                InputProps={{
-                    endAdornment: (
-                        searchQuery && (
-                            <IconButton
-                                aria-label="clear search"
-                                onClick={handleClear}
-                                edge="end"
-                                size="small"
-                            >
-                                <ClearIcon />
-                            </IconButton>
-                        )
-                    )
-                }}
-            />
+        <div>
+
         </div>
     );
 };
